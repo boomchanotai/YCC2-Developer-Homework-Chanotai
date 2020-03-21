596d265e4086b086f4b8b09f5c185f0a39d2d726
Chanotai Krajeam <boom9387@hotmail.com>
2020-03-21 10:52:33 +0700

    This is my code.

---
+++ js/app.js
@@ -0,0 +1,303 @@
+$(document).ready(function(){
+    $(".back").click(function(){
+        $("#surface-container").fadeOut()
+        $("#swift-container").fadeOut()
+        $("#xps-container").fadeOut()
+        $("#zenbookpro-container").fadeOut()
+        $("#macbook-container").fadeOut()
+        $("#index-body").fadeIn()
+    })
+    $("#macbook-info").click(function(){
+        $("#finish").fadeOut()
+        $("#macbook-container").fadeIn()
+        $("#index-body").hide()
+        $('html, body').animate({
+            scrollTop: $("#macbook-container").offset().top
+        }, 0);
+        $("#macbook").click(function(){
+            $("#finish").fadeOut()
+            $("#surface-container").fadeOut()
+            $("#swift-container").fadeOut()
+            $("#xps-container").fadeOut()
+            $("#zenbookpro-container").fadeOut()
+            $("#macbook-container").fadeIn()
+        })
+        $("#surface").click(function(){
+            $("#finish").fadeOut()
+            $("#macbook-container").fadeOut()
+            $("#swift-container").fadeOut()
+            $("#xps-container").fadeOut()
+            $("#zenbookpro-container").fadeOut()
+            $("#surface-container").fadeIn()
+        })
+        $("#swift").click(function(){
+            $("#finish").fadeOut()
+            $("#macbook-container").fadeOut()
+            $("#surface-container").fadeOut()
+            $("#xps-container").fadeOut()
+            $("#zenbookpro-container").fadeOut()
+            $("#swift-container").fadeIn()
+        })
+        $("#xps").click(function(){
+            $("#finish").fadeOut()
+            $("#macbook-container").fadeOut()
+            $("#surface-container").fadeOut()
+            $("#swift-container").fadeOut()
+            $("#zenbookpro-container").fadeOut()
+            $("#xps-container").fadeIn()
+        })
+        $("#zenbook").click(function(){
+            $("#finish").fadeOut()
+            $("#macbook-container").fadeOut()
+            $("#surface-container").fadeOut()
+            $("#swift-container").fadeOut()
+            $("#xps-container").fadeOut()
+            $("#zenbookpro-container").fadeIn()
+        })
+    })
+    $("#surface-info").click(function(){
+        $("#finish").fadeOut()
+        $("#surface-container").fadeIn()
+        $("#index-body").hide()
+        $('html, body').animate({
+            scrollTop: $("#surface-container").offset().top
+        }, 0);
+        $("#macbook").click(function(){
+            $("#finish").fadeOut()
+            $("#surface-container").fadeOut()
+            $("#swift-container").fadeOut()
+            $("#xps-container").fadeOut()
+            $("#zenbookpro-container").fadeOut()
+            $("#macbook-container").fadeIn()
+        })
+        $("#surface").click(function(){
+            $("#finish").fadeOut()
+            $("#macbook-container").fadeOut()
+            $("#swift-container").fadeOut()
+            $("#xps-container").fadeOut()
+            $("#zenbookpro-container").fadeOut()
+            $("#surface-container").fadeIn()
+        })
+        $("#swift").click(function(){
+            $("#finish").fadeOut()
+            $("#macbook-container").fadeOut()
+            $("#surface-container").fadeOut()
+            $("#xps-container").fadeOut()
+            $("#zenbookpro-container").fadeOut()
+            $("#swift-container").fadeIn()
+        })
+        $("#xps").click(function(){
+            $("#finish").fadeOut()
+            $("#macbook-container").fadeOut()
+            $("#surface-container").fadeOut()
+            $("#swift-container").fadeOut()
+            $("#zenbookpro-container").fadeOut()
+            $("#xps-container").fadeIn()
+        })
+        $("#zenbook").click(function(){
+            $("#finish").fadeOut()
+            $("#macbook-container").fadeOut()
+            $("#surface-container").fadeOut()
+            $("#swift-container").fadeOut()
+            $("#xps-container").fadeOut()
+            $("#zenbookpro-container").fadeIn()
+        })
+    })
+    $("#swift-info").click(function(){
+        $("#finish").fadeOut()
+        $("#swift-container").fadeIn()
+        $("#index-body").hide()
+        $('html, body').animate({
+            scrollTop: $("#swift-container").offset().top
+        }, 0);
+        $("#macbook").click(function(){
+            $("#finish").fadeOut()
+            $("#surface-container").fadeOut()
+            $("#swift-container").fadeOut()
+            $("#xps-container").fadeOut()
+            $("#zenbookpro-container").fadeOut()
+            $("#macbook-container").fadeIn()
+        })
+        $("#surface").click(function(){
+            $("#finish").fadeOut()
+            $("#macbook-container").fadeOut()
+            $("#swift-container").fadeOut()
+            $("#xps-container").fadeOut()
+            $("#zenbookpro-container").fadeOut()
+            $("#surface-container").fadeIn()
+        })
+        $("#swift").click(function(){
+            $("#finish").fadeOut()
+            $("#macbook-container").fadeOut()
+            $("#surface-container").fadeOut()
+            $("#xps-container").fadeOut()
+            $("#zenbookpro-container").fadeOut()
+            $("#swift-container").fadeIn()
+        })
+        $("#xps").click(function(){
+            $("#finish").fadeOut()
+            $("#macbook-container").fadeOut()
+            $("#surface-container").fadeOut()
+            $("#swift-container").fadeOut()
+            $("#zenbookpro-container").fadeOut()
+            $("#xps-container").fadeIn()
+        })
+        $("#zenbook").click(function(){
+            $("#finish").fadeOut()
+            $("#macbook-container").fadeOut()
+            $("#surface-container").fadeOut()
+            $("#swift-container").fadeOut()
+            $("#xps-container").fadeOut()
+            $("#zenbookpro-container").fadeIn()
+        })
+    })
+    $("#xps-info").click(function(){
+        $("#finish").fadeOut()
+        $("#xps-container").fadeIn()
+        $("#index-body").hide()
+        $('html, body').animate({
+            scrollTop: $("#xps-container").offset().top
+        }, 0);
+        $("#macbook").click(function(){
+            $("#finish").fadeOut()
+            $("#surface-container").fadeOut()
+            $("#swift-container").fadeOut()
+            $("#xps-container").fadeOut()
+            $("#zenbookpro-container").fadeOut()
+            $("#macbook-container").fadeIn()
+        })
+        $("#surface").click(function(){
+            $("#finish").fadeOut()
+            $("#macbook-container").fadeOut()
+            $("#swift-container").fadeOut()
+            $("#xps-container").fadeOut()
+            $("#zenbookpro-container").fadeOut()
+            $("#surface-container").fadeIn()
+        })
+        $("#swift").click(function(){
+            $("#finish").fadeOut()
+            $("#macbook-container").fadeOut()
+            $("#surface-container").fadeOut()
+            $("#xps-container").fadeOut()
+            $("#zenbookpro-container").fadeOut()
+            $("#swift-container").fadeIn()
+        })
+        $("#xps").click(function(){
+            $("#finish").fadeOut()
+            $("#macbook-container").fadeOut()
+            $("#surface-container").fadeOut()
+            $("#swift-container").fadeOut()
+            $("#zenbookpro-container").fadeOut()
+            $("#xps-container").fadeIn()
+        })
+        $("#zenbook").click(function(){
+            $("#finish").fadeOut()
+            $("#macbook-container").fadeOut()
+            $("#surface-container").fadeOut()
+            $("#swift-container").fadeOut()
+            $("#xps-container").fadeOut()
+            $("#zenbookpro-container").fadeIn()
+        })
+    })
+    $("#zenbookpro-info").click(function(){
+        $("#finish").fadeOut()
+        $("#zenbookpro-container").fadeIn()
+        $("#index-body").hide()
+        $('html, body').animate({
+            scrollTop: $("#zenbookpro-container").offset().top
+        }, 0);
+        $("#macbook").click(function(){
+            $("#finish").fadeOut()
+            $("#surface-container").fadeOut()
+            $("#swift-container").fadeOut()
+            $("#xps-container").fadeOut()
+            $("#zenbookpro-container").fadeOut()
+            $("#macbook-container").fadeIn()
+        })
+        $("#surface").click(function(){
+            $("#finish").fadeOut()
+            $("#macbook-container").fadeOut()
+            $("#swift-container").fadeOut()
+            $("#xps-container").fadeOut()
+            $("#zenbookpro-container").fadeOut()
+            $("#surface-container").fadeIn()
+        })
+        $("#swift").click(function(){
+            $("#finish").fadeOut()
+            $("#macbook-container").fadeOut()
+            $("#surface-container").fadeOut()
+            $("#xps-container").fadeOut()
+            $("#zenbookpro-container").fadeOut()
+            $("#swift-container").fadeIn()
+        })
+        $("#xps").click(function(){
+            $("#finish").fadeOut()
+            $("#macbook-container").fadeOut()
+            $("#surface-container").fadeOut()
+            $("#swift-container").fadeOut()
+            $("#zenbookpro-container").fadeOut()
+            $("#xps-container").fadeIn()
+        })
+        $("#zenbook").click(function(){
+            $("#finish").fadeOut()
+            $("#macbook-container").fadeOut()
+            $("#surface-container").fadeOut()
+            $("#swift-container").fadeOut()
+            $("#xps-container").fadeOut()
+            $("#zenbookpro-container").fadeIn()
+        })
+    })
+    $("#browse").click(function(){
+        $("#finish").fadeOut()
+        $("#index-body").fadeIn()
+    })
+    $(".purchase").click(function(){
+        $("#macbook-container").fadeOut()
+        $("#surface-container").fadeOut()
+        $("#swift-container").fadeOut()
+        $("#xps-container").fadeOut()
+        $("#zenbookpro-container").fadeOut()
+        $("#index-body").hide()
+        $("#finish").fadeIn()
+    })
+    if($(window).width() < 700){
+        $("#nav-item").hide()
+        $("#navbar").click(function(){
+            $("#nav-item").fadeToggle()
+        })
+    }
+    if($("#index-body").css("display") != "none"){
+        $("#macbook").click(function(){
+            $('html, body').animate({
+                scrollTop: $("#macbookpro-preview").offset().top
+            }, 800);
+        })
+        $("#surface").click(function(){
+            $('html, body').animate({
+                scrollTop: $("#SurfaceSwift-preview").offset().top - 40
+            }, 800);
+        })
+        $("#swift").click(function(){
+            $('html, body').animate({
+                scrollTop: $("#SurfaceSwift-preview").offset().top - 40
+            }, 800);
+        })
+        $("#xps").click(function(){
+            $('html, body').animate({
+                scrollTop: $("#XPS-Zenbook-preview").offset().top - 40
+            }, 800);
+        })
+        $("#zenbook").click(function(){
+            $('html, body').animate({
+                scrollTop: $("#XPS-Zenbook-preview").offset().top - 40
+            }, 800);
+        })
+    }
+    $.each($(".ssd"), function(){
+        $(this).click(function(){
+            $(".ssd").css("background-color", "").css("border", "").css("color", "")
+            $(this).css("background-color", "#007AFF1A")
+            .css("border", "1.2px solid #007AFF").css("color", "#007AFF")
+        })
+    })
+})
